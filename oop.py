966a52fd73d2951d43db99a14824064e2e96ebbe
JAmalMichael <eshimi090@gmail.com>
2024-08-21 10:54:07 -1200

    constructor

--- oop.py
+++ oop.py
@@ -7,30 +7,47 @@ class Phone:
 
 
 
-class Phone1:
-    def set_color(self, color):
-        self.color = color
+# class Phone1:
+#     def set_color(self, color):
+#         self.color = color
 
-    def set_cost(self, cost):
-        self.cost = cost
+#     def set_cost(self, cost):
+#         self.cost = cost
         
-    def show_color(self):
-        return self.color
+#     def show_color(self):
+#         return self.color
         
-    def show_cost(self):
-        return self.cost
+#     def show_cost(self):
+#         return self.cost
         
-    def play_game(self):
-        print("Playing a game")
+#     def play_game(self):
+#         print("Playing a game")
         
-    def make_call(self):
-        print("making a call")
+#     def make_call(self):
+#         print("making a call")
 
-# Creating an instance of Phone1
-p2 = Phone1()
+# # Creating an instance of Phone1
+# p2 = Phone1()
 
-# Setting the cost of the phone
-p2.set_cost(500)
+# # Setting the cost of the phone
+# p2.set_cost(500)
 
-# Printing the cost of the phone
-print(p2.show_cost())  # Output: 500
+# # Printing the cost of the phone
+# print(p2.show_cost())  # Output: 500
+
+
+class Employee:
+    def __init__(self, name, age, salary, gender):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.gender = gender
+
+    def show_details(self):
+        print("The name of Employee is", self.name)
+        print("The age of Employee is", self.age)
+        print("The salary of Employee is", self.salary)
+        print("The gender of Employee is", self.gender)
+
+e1 = Employee('Julia',25,11200,'Female')
+e1.show_details()
