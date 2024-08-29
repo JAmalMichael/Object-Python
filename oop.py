0341b0a04f24a9e4410f17f398f5c1064dd77bcb
JAmalMichael <eshimi090@gmail.com>
2024-08-28 21:35:39 -1200

    Gui update

--- oop.py
+++ oop.py
@@ -12,8 +12,28 @@ def main_screen():
     screen.title("EDmessageApp")
 
     Label(text="Enter text for encryption and decryption", 
-          fg="black", font=("Times New Roman", 13)).place(x=10,  y=10)
+          fg="black", font=("Calibri", 13)).place(x=10,  y=10)
+     
+    text1 = Text(font="Roboto 20", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+    text1.place(x=10, y=50, width=355, height=100)
 
+    Label(text="Enter secret key for encryption and decryption", fg="black", 
+    font=("calibri", 13)).place(x=10, y=170)
+
+    code = StringVar()
+    Entry(textvariable=code, width=19, bd=0, font=("arial", 25), show="*").place(x=10, y=200)
+
+    Button(text="ENCRYPT", height=2, width=23, bg="#ed3833", fg="white", bd=0).place(x=10, y=250)
+    Button(text="DECRYPT", height=2, width=23, bg="#00bd56", fg="white", bd=0).place(x=200, y=250)
+    Button(text="RESET", height=2, width=50, bg="#1089ff", fg="white", bd=0).place(x=10, y=300)
+
+
+    
+    
+    
+    
+    
     screen.mainloop()
+
 
 main_screen()
